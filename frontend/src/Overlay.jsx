385cfe05bb62ff78ac36b5702b11df824c57a575
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2023-05-29 18:36:38 +0300

    004

--- frontend/src/Overlay.jsx
+++ frontend/src/Overlay.jsx
@@ -2,7 +2,28 @@ import React from "react"
 
 const Overlay = function ()
 {
+    // states
 
+    const [state_message, set_state_message] = React.useState(null)
+
+    ///
+
+    if (state_overlay_message === null) return
+
+    ///
+
+    return <div
+        style=
+        {
+            {
+                position: "fixed",
+                inset: "0 0 0 0",
+                backgroundColor: "rgba(80, 80, 80, 0.80)"
+            }
+        }>
+        {state_overlay_message}
+    </div>
 }
 
+export { set_state_message }
 export default Overlay
