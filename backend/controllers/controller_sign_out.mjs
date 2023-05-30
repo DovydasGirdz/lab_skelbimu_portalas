707f1d258df7eb8e8dc4363f4b871739ac81e256
vardenis_pavardenis <epmc.vardenis.pavardenis@gmail.com>
2023-05-30 19:38:29 +0300

    006

---
+++ backend/controllers/controller_sign_out.mjs
@@ -0,0 +1,77 @@
+import config_users from "../config/config_users.mjs"
+import model_users_read from "../models/model_users_read.mjs"
+import model_users_update from "../models/model_users_update.mjs"
+
+
+const controller_sign_out = async function (req, res)
+{
+    // inputs
+
+    const identification_token = req.cookies.identification_token
+
+    try
+    {
+        // validate_inputs
+
+        validate_inputs(identification_token)
+
+        // resolve username
+
+        const result_of_resolve_username = await resolve_username(identification_token)
+
+        // model_users_update
+
+        await model_users_update(
+            {
+                username: result_of_resolve_username
+            },
+            {
+                identification_token: null
+            }
+        )
+
+        // success
+
+        res.cookie("identification_token", null, { maxAge: 0 })
+        res.statusCode = 200
+        res.end()
+
+    }
+    catch (error)
+    {
+        res.statusCode = 400
+        res.json({ message: error.message })
+    }
+}
+
+//
+// validate_inputs
+//
+
+const validate_inputs = function (identification_token)
+{
+    config_users.validate_identification_token(identification_token)
+}
+
+// 
+// resolve username
+//
+
+const resolve_username = async function (param_identification_token)
+{
+    const result_of_model_users_read = 
+        await model_users_read(
+            {
+                identification_token: param_identification_token
+            },
+            {
+                _id: 0,
+                username: 1
+            }
+        )
+
+    const username = result_of_model_users_read.username
+    return username
+}
+
+export default controller_sign_out
